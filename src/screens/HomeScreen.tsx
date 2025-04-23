436bbc7a0eee3d34e181bdccc1fa9c8a9554df6d
farid <valkanid12@gmail.com>
2025-04-24 01:49:57 +0700

    stats user

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import colors from "../styles/colors";
 import globalStyles from "../styles/globalStyles";
 import CustomPicker from "../components/common/CustomPicker";
 import { useSelector } from "react-redux";
+import { ScrollView } from "react-native-gesture-handler";
 
 type NutritionBarProps = {
   label: string;
@@ -14,9 +15,6 @@ type NutritionBarProps = {
 };
 
 const NutritionBar: React.FC<NutritionBarProps> = ({ label, value, color, limit }) => {
-  const user = useSelector((state: any) => state.auth.user);
-
-  console.log(user);
   const percentage = (value / limit) * 100;
   return (
     <View>
@@ -36,11 +34,28 @@ const NutritionBar: React.FC<NutritionBarProps> = ({ label, value, color, limit 
 const HomeScreen = () => {
   const [selectedUser, setSelectedUser] = useState("User 1");
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const user = useSelector((state: any) => state.auth.user);
 
   const users = [
-    { label: "User 1", value: "User 1", imageUrl: "https://static.vecteezy.com/system/resources/previews/019/896/008/non_2x/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png" },
+    { label: user.name, value: user.name, imageUrl: "https://static.vecteezy.com/system/resources/previews/019/896/008/non_2x/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png" },
     { label: "User 2", value: "User 2", imageUrl: "https://static.vecteezy.com/system/resources/previews/019/896/012/non_2x/female-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png" },
   ];
+
+  const findNutritionByDate = (date: Date) => {
+    const targetDate = date.toISOString().split("T")[0];
+
+    const nutritionData = user.nutrition_stats.find((item: any) => {
+      const itemDate = item.date.split("T")[0];
+      return itemDate === targetDate;
+    });
+
+    return nutritionData || null;
+  };
+
+  const formatDate = (date: any) => new Date(date).toISOString().split("T")[0];
+  const filteredLogs = user.meal_logs?.find((log: any) => formatDate(log.date) === formatDate(selectedDate));
+
+  const nutritionData = findNutritionByDate(selectedDate);
 
   const changeDate = (days: number) => {
     const newDate = new Date(selectedDate);
@@ -54,33 +69,34 @@ const HomeScreen = () => {
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Harian</Text>
         <View style={styles.profileContainer}>
-          <CustomPicker selectedValue={selectedUser} onValueChange={(itemValue) => setSelectedUser(itemValue)} items={users} />
+          <Text style={styles.sectionTitle}>{user.name}</Text>
+          <Image style={styles.selectedItemImage} source={{ uri: "https://static.vecteezy.com/system/resources/previews/019/896/008/non_2x/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png" }}></Image>
         </View>
       </View>
 
       {/* Rounded Rectangle */}
       <View style={styles.roundedContainer}>
         <View style={styles.rectangle}>
-          <Text style={{ color: colors.background }}>0 Kal</Text>
+          <Text style={{ color: colors.background }}>{nutritionData?.total_calories.toFixed() ?? 0}</Text>
         </View>
         <View style={styles.circle}>
-          <Text style={{ color: colors.text }}>2500</Text>
+          <Text style={{ color: colors.text }}>{user.daily_nutrition_target?.calories}</Text>
         </View>
       </View>
 
       {/* Nutrition Bars */}
       <View style={styles.nutritionContainer}>
         <View style={styles.barGroup}>
-          <NutritionBar label="Karbohidrat" value={70} color="#FF6666" limit={225} />
-          <NutritionBar label="Lemak" value={50} color="#FFD700" limit={75} />
-          <NutritionBar label="Protein" value={80} color="#66CCFF" limit={80} />
+          <NutritionBar label="Karbohidrat" value={nutritionData?.total_carbs.toFixed() ?? 0} color="#FF6666" limit={user.daily_nutrition_target?.carbs} />
+          <NutritionBar label="Lemak" value={nutritionData?.total_fat.toFixed() ?? 0} color="#FFD700" limit={user.daily_nutrition_target?.protein} />
+          <NutritionBar label="Protein" value={nutritionData?.total_protein.toFixed() ?? 0} color="#66CCFF" limit={user.daily_nutrition_target?.protein} />
         </View>
       </View>
       <View style={styles.nutritionContainer}>
         <View style={styles.barGroup}>
-          <NutritionBar label="Serat" value={70} color="#FF6666" limit={225} />
-          <NutritionBar label="Gula" value={50} color="#FFD700" limit={75} />
-          <NutritionBar label="Garam" value={80} color="#66CCFF" limit={80} />
+          <NutritionBar label="Serat" value={nutritionData?.total_fiber.toFixed() ?? 0} color="#FF6666" limit={user.daily_nutrition_target?.fiber} />
+          <NutritionBar label="Gula" value={nutritionData?.total_sugar.toFixed() ?? 0} color="#FFD700" limit={user.daily_nutrition_target?.sugar} />
+          <NutritionBar label="Garam" value={nutritionData?.total_sodium.toFixed() ?? 0} color="#66CCFF" limit={user.daily_nutrition_target?.sodium} />
         </View>
       </View>
 
@@ -108,30 +124,58 @@ const HomeScreen = () => {
       </View>
 
       {/* Table */}
-      <View style={styles.table}>
-        <View style={styles.tableTextContainer}>
-          <Text style={styles.totalEnergy}>Total Energi: 0 kcal</Text>
-          <AntDesign name="up" size={24} color="black" />
-        </View>
-        <View style={styles.separator} />
-        <View style={styles.tableRow}>
-          <Text style={styles.tableHeader}>Nama</Text>
-          <Text style={styles.tableHeader}>Jumlah</Text>
-          <Text style={styles.tableHeader}>kcal</Text>
-          <Text style={styles.tableHeader}>Karbo</Text>
-          <Text style={styles.tableHeader}>Protein</Text>
-          <Text style={styles.tableHeader}>Serat</Text>
-        </View>
-      </View>
+      <ScrollView>
+        {filteredLogs?.meals.map((meal: any, index: any) => (
+          <View key={index} style={styles.table}>
+            <View style={styles.tableTextContainer}>
+              <AntDesign name="up" size={24} color="black" />
+              <Text style={styles.totalEnergy}>{meal.name}</Text>
+            </View>
+
+            <View style={styles.separator} />
+
+            <ScrollView horizontal>
+              <View style={[styles.tableContent]}>
+                {/* Header */}
+                <View style={styles.tableRow}>
+                  <Text style={styles.tableHeader}>Kalori</Text>
+                  <Text style={styles.tableHeader}>Karbo</Text>
+                  <Text style={styles.tableHeader}>Protein</Text>
+                  <Text style={styles.tableHeader}>Lemak</Text>
+                  <Text style={styles.tableHeader}>Serat</Text>
+                  <Text style={styles.tableHeader}>Gula</Text>
+                  <Text style={styles.tableHeader}>Garam</Text>
+                </View>
+
+                {/* Data meal */}
+                <View style={styles.tableRow}>
+                  <Text style={styles.tableCell}>{meal.nutrition_info.calories.toFixed()}</Text>
+                  <Text style={styles.tableCell}>{meal.nutrition_info.carbs.toFixed()}</Text>
+                  <Text style={styles.tableCell}>{meal.nutrition_info.protein.toFixed()}</Text>
+                  <Text style={styles.tableCell}>{meal.nutrition_info.fat.toFixed()}</Text>
+                  <Text style={styles.tableCell}>{meal.nutrition_info.fiber.toFixed()}</Text>
+                  <Text style={styles.tableCell}>{meal.nutrition_info.sugar.toFixed()}</Text>
+                  <Text style={styles.tableCell}>{meal.nutrition_info.sodium.toFixed()}</Text>
+                </View>
+              </View>
+            </ScrollView>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  selectedItemImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
   container: { flex: 1, padding: 20, backgroundColor: colors.background },
   header: { flexDirection: "row", justifyContent: "space-between", alignItems: "center" },
   headerTitle: { fontSize: 20, fontWeight: "bold" },
-  profileContainer: { flexDirection: "row", alignItems: "center" },
+  profileContainer: { flexDirection: "row", alignItems: "center", gap: 16, justifyContent: "center" },
   profileImage: { width: 40, height: 40, borderRadius: 20, marginRight: 10 },
   picker: { height: 40, width: 120 },
   roundedContainer: { flexDirection: "row", alignItems: "center", marginVertical: 20 },
@@ -158,11 +202,20 @@ const styles = StyleSheet.create({
   image: { height: 10, aspectRatio: 1 },
   dateSelector: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginVertical: 10 },
   table: { marginTop: 20, borderWidth: 1, borderColor: "#ddd", padding: 10, backgroundColor: colors.background },
-  tableTextContainer: { flexDirection: "row", justifyContent: "space-between" },
+  tableTextContainer: { flexDirection: "row", gap: 16 },
   totalEnergy: { fontWeight: "bold", fontSize: 16, textAlign: "center" },
   separator: { borderBottomWidth: 1, borderColor: "#ddd", marginVertical: 5 },
-  tableRow: { flexDirection: "row", justifyContent: "space-between" },
+  tableRow: { flexDirection: "row", justifyContent: "space-between", width: "100%", gap: 16 },
   tableHeader: { fontWeight: "bold", flex: 1, textAlign: "center" },
+  tableCell: {
+    flex: 1,
+    textAlign: "center",
+  },
+  tableContent: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
 });
 
 export default HomeScreen;
